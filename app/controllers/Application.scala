4ab80f56cb35bfee0c74b5ab1ddb5e694b58cc31
Nicolas A Perez <anicolaspp@gmail.com>
2016-02-03 00:12:59 -0500

    Update Application.scala

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,7 +6,7 @@ import play.api.mvc._
 class Application extends Controller {
 
   def index = Action {
-    Ok(views.html.index("Testing auto deployment"))
+    Ok(views.html.index("Testing auto deployment in heroku"))
   }
 
 }
